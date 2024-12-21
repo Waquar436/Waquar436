b0a1990052272b4869bc7aa6bcb26f8b6edb10a0
waquarahmad <waquar30@gmail.com>
2024-12-21 22:47:12 +0530

    21-12-2024

--- day3_promode.py
+++ day3_promode.py
@@ -4,4 +4,16 @@ c1 = "C"
 #raw string ignores special character
 
 dir = r'C:\pWaquar\t.txt'
-print(dir)
+# print(dir)
+
+num1 = int(input("print 1st num"))
+num2 = int(input("print 2nd num"))
+num3 = int(input("print 3rd num"))
+res = num1 - num2 - num3
+# print(res)
+
+res1 = num1%num2
+res2 = num1//8
+num2
+print(res1)
+print(num2)
