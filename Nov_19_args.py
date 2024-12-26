a60f7b322eac94fde82de91240dc876a5ad4cdd7
waquarahmad <waquar30@gmail.com>
2024-12-26 19:46:22 +0530

    26-12-2024

--- Nov_19_args.py
+++ Nov_19_args.py
@@ -3,4 +3,10 @@ def print_mul_arguments(*args):
     for i in args:
         print(i)
 
+def make_pizza(*toppings):
+    # *args -> List
+    for i in toppings:
+        print(i)
+
 print_mul_arguments(1,2,3,4,5,6,"Ahmad")
+make_pizza("corn","onion")
