c494628dcf2f9b63fe137088441259d254954d6d
waquarahmad <waquar30@gmail.com>
2024-12-27 08:33:08 +0530

    27-12-2024

--- Nov_23_l ambda_with_multiple_stmnts.py
+++
@@ -1,7 +0,0 @@
-import math
-
-check_even_odd = lambda  n : "even" if n%2 ==0 else "odd"
-print(check_even_odd(8))
-
-find_sqr = lambda  : math.pow(int(input("enter the number")),2)
-print(find_sqr())
