bb18d141c68134bdc741f6e81f68629731fd77ce
waquarahmad <waquar30@gmail.com>
2024-12-28 08:58:42 +0530

    28-12-2024

--- test_pytest_learning1.py
+++ test_pytest_learning1.py
@@ -6,7 +6,7 @@ import pytest
 def test_method1():
     print("This is pytest method.")
     assert 1-1 == 2
-@pytest.mark.smoke
+@pytest.mark.regression
 def test_method2():
     print("test2")
     assert 1+1 == 2
