a545900fdb53ef4c7d7bbdbd5649ac2b93fa054e
waquarahmad <waquar30@gmail.com>
2024-12-31 15:51:07 +0530

    31-12-2024

--- Nov_30_constructor.py
+++ Nov_30_constructor.py
@@ -1,7 +1,20 @@
 from dotenv import load_dotenv
 import os
-load_dotenv()
-email = os.getenv("EMAIL")
-pwd = os.getenv("PASSWORD")
-print(email)
-print(pwd)
+# load_dotenv()
+# email = os.getenv("EMAIL")
+# pwd = os.getenv("PASSWORD")
+# print(email)
+# print(pwd)
+
+class Car:
+    def __init__(self):
+        self.password = "Ahmad"
+        self.__password_secure = "pass123"
+
+    def change_pwd(self):
+        print(self.password)
+
+object_ref = Car()
+print(object_ref.password)
+# print(object_ref.__password_secure)
+print(object_ref.change_pwd())
