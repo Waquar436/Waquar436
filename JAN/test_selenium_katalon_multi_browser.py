d3037fbdba7bab711f65dc02238d62066706f313
waquarahmad <waquar30@gmail.com>
2025-01-18 16:01:17 +0530

    18-01-2025

--- JAN/test_selenium_katalon_multi_browser.py
+++ JAN/test_selenium_katalon_multi_browser.py
@@ -1,35 +1,26 @@
 import pytest
 import allure
 from selenium import webdriver
+import time
 
 
 @allure.title("verify that the content of katalon-demo-cura is expected on multiple browser")
 def test_katalon_chrome():
     driver = webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
-    if "CURA Healthcare Service" in driver.page_source:
-        print("Test case passed.")
-    else:
-        print("Test case failed.")
-        pytest.fail("Test not found on the page")
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
 
 def test_katalon_edge():
     driver = webdriver.Edge()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
-    if "CURA Healthcare Service" in driver.page_source:
-        print("Test case passed.")
-    else:
-        print("Test case failed.")
-        pytest.fail("Test not found on the page")
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
 
 def test_katalon_firefox():
     driver = webdriver.Firefox()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
-    if "CURA Healthcare Service" in driver.page_source:
-        print("Test case passed.")
-    else:
-        print("Test case failed.")
-        pytest.fail("Test not found on the page")
-
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
+    time.sleep(5)
+    # driver.close()
+    driver.quit()
 
 
