ad6cd949bd13a888c853bfc795c5aada774ec75e
waquarahmad <waquar30@gmail.com>
2025-01-24 23:08:22 +0530

    23-01-2025

---
+++ JAN/test_webtables.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_webtables():
+    driver=webdriver.Chrome()
+    driver.get("https://awesomeqa.com/webtable.html")
+    driver.maximize_window()
+    #Row
+    #//table[contains(@id,"cust")]/tbody/tr
+    row_elements = driver.find_elements(By.XPATH,"//table[contains(@id,'cust')]/tbody/tr")
+    row=len(row_elements)
+    print(row)
+
+    #Column
+    #//table[contains(@id,"cust")]/tbody/tr[2]/td
+    col_elements = driver.find_elements(By.XPATH,"//table[contains(@id,'cust')]/tbody/tr[2]/td")
+    col=len(col_elements)
+    print(col)
+
+    #Dynamic XPath
+    first_part="//table[contains(@id,'cust')]/tbody/tr["
+    second_part="]/td["
+    third_part="]"
+    for i in range(2,row+1):
+        for j in range(1,col+1):
+            dynamic_path=f"{first_part}{i}{second_part}{j}{third_part}"
+            # print(dynamic_path)
+            print(driver.find_element(By.XPATH,dynamic_path).text,end="\n")
+
