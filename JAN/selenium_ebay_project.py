952097ebbf68641150349483971c550f67f050e2
waquarahmad <waquar30@gmail.com>
2025-01-28 23:55:12 +0530

    28-01-2025

--- JAN/selenium_ebay_project.py
+++ JAN/selenium_ebay_project.py
@@ -8,4 +8,10 @@ import allure
 @allure.description("verify that 62 items are there for macmini")
 
 def test_ebay():
-    print("do it")
+    driver=webdriver.Chrome()
+    driver.get("https://www.ebay.com/sch/i.html?_nkw=macmini&_sacat=0&_from=R40&_trksid=p4432023.m570.l1313")
+    search_box_input_path=driver.find_element(By.XPATH,"//input[@placeholder='Search for anything']")
+    search_box_input_path.send_keys("macmini")
+    search_btn=driver.find_element(By.CSS_SELECTOR,"#gh-search-btn > span")
+
+
